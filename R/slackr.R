9ac932de1f9db9bad7af8adece6350ed86073923
Bob Rudis <bob@rudis.net>
2014-09-04 09:16:23 -0400

    nearly ready for release

--- R/slackr.R
+++ R/slackr.R
@@ -38,15 +38,14 @@ slackrSetup <- function(channel="#general", username="slackr",
 #'
 #' By default, everyting but \code{expr} will be looked for in a "\code{SLACK_}"
 #' environment variable. You can override or just specify these values directly instead,
-#' but it's probably better to call `slackrSetup()` first.
+#' but it's probably better to call \code{slackrSetup()} first.
 #'
-#' @param expr an R expression to be evaluated
+#' @param ... expressions to be sent to Slack.com
 #' @param channel which channel to post the message to (chr)
 #' @param username what user should the bot be named as (chr)
 #' @param icon_emoji what emoji to use (chr) \code{""} will mean use the default
 #' @param slack_webhook_url_prefix which slack.com API endpoint URL to use
 #' @param token your webhook API token
-#' @return if there was an error posting to Slack, the \code{POST} response object will be returned
 #' @import httr
 #' @examples
 #' \dontrun{
@@ -55,7 +54,7 @@ slackrSetup <- function(channel="#general", username="slackr",
 #' slackr(str(iris))
 #' }
 #' @export
-slackr <- function(expr,
+slackr <- function(...,
                    channel=Sys.getenv("SLACK_CHANNEL"),
                    username=Sys.getenv("SLACK_USERNAME"),
                    icon_emoji=Sys.getenv("SLACK_ICON_EMOJI"),
@@ -66,27 +65,47 @@ slackr <- function(expr,
     stop("No URL prefix and/or token specified. Did you forget to call slackrSetup()?", call. = FALSE)
   }
 
-  if (missing(expr)) {
-    stop('"expr" must be specified', call. = FALSE)
-  }
-
-  data <- capture.output(eval(expr))
-  data <- paste0(data, collapse="\n")
-
-  output <- gsub('^\"|\"$', "", toJSON(data, simplifyVector=TRUE, flatten=TRUE, auto_unbox=TRUE))
-
   if (icon_emoji != "") { icon_emoji <- sprintf(', "icon_emoji": "%s"', icon_emoji)  }
 
-  resp <- POST(url=paste0(slack_webhook_url_prefix, "token=", token),
-               add_headers(`Content-Type`="application/x-www-form-urlencoded", `Accept`="*/*"),
-               body=URLencode(sprintf('payload={"channel": "%s", "username": "%s", "text": "```%s```"%s}',
-                                      channel, username, output, icon_emoji)))
+  resp_ret <- ""
 
-  if (resp$status > 200) {
-    message(resp$message)
-    return(resp)
+  if (!missing(...)) {
+
+    input_list <- as.list(substitute(list(...)))[-1L]
+
+    for(i in 1:length(input_list)) {
+
+      expr <- input_list[[i]]
+
+      if (class(expr) == "call") {
+
+        expr_text <- sprintf("> %s", deparse(expr))
+
+        data <- capture.output(eval(expr))
+        data <- paste0(data, collapse="\n")
+        data <- sprintf("%s\n%s", expr_text, data)
+
+      } else {
+        data <- as.character(expr)
+      }
+
+      output <- gsub('^\"|\"$', "", toJSON(data, simplifyVector=TRUE, flatten=TRUE, auto_unbox=TRUE))
+
+      resp <- POST(url=paste0(slack_webhook_url_prefix, "token=", token),
+                   add_headers(`Content-Type`="application/x-www-form-urlencoded", `Accept`="*/*"),
+                   body=URLencode(sprintf('payload={"channel": "%s", "username": "%s", "text": "```%s```"%s}',
+                                          channel, username, output, icon_emoji)))
+
+      warn_for_status(resp)
+
+      if (resp$status_code > 200) { print(str(expr))}
+
+    }
+
   }
 
   return(invisible())
 
 }
+
+
