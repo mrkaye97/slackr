0031e832d470a7c5ea726779a9e52894d2cf3451
Daniel P. Egan <dp.egan@gmail.com>
2019-02-07 22:11:42 -0500

    remove test: need to manage environment vars more carefully.

--- tests/testthat/test-slackr.R
+++ tests/testthat/test-slackr.R
@@ -1,54 +1,6 @@
-library(yaml)
-library(testthat)
-library(slackr)
+context("basic functionality")
+test_that("we can do something", {
 
-creds <- yaml::yaml.load_file(input = "~/src/slackr_creds.yml")
-
-context("basic functioning")
-
-test_that("Webhook fails/works appropriately", {
-  slackr_setup(channel = "#publicchanneltest",
-               incoming_webhook_url = creds$slack_bot$invalid$webhook,
-               username = "slackr_bot")
-
-  expect_warning(slackr_bot(txt = "testing 1,2,3"))
-
-  Sys.sleep(1)
-  slackr_setup(channel = "#publicchanneltest",
-               incoming_webhook_url = creds$slack_bot$valid$webhook,
-               username = "slackr_bot")
-
-  expect_error(slackr_bot(txt = "testing 1,2,3"), NA)
-
-})
-
-
-test_that("Valid api tokens work", {
-  # On github, should be done with Env variable.
-
-  slackr_setup(channel = "#publicchanneltest",
-               api_token = creds$slack_bot$valid$api_token,
-               username = "slackr_bot",
-               icon_emoji = "thumbsup")
-
-  # Does not respect username, icon, etc.
-  expect_error(
-    slackr::slackr_msg(txt = paste("testing ", Sys.time())),
-               regexp = NA)
-  Sys.sleep(1)
-  expect_error(res <- slackr::slackr_msg(txt = "testing 1,2,3",
-                            channel = "@dp.egan"),
-               regexp = NA)
-  Sys.sleep(1)
-  expect_error(text_slackr(text = "testing text_slackr",
-                             icon_emoji = "robot",
-                             username = "cashhubbot",
-                             channel = "@dp.egan"),
-               regexp = NA)
-  Sys.sleep(1)
-  expect_error(slackr_upload(filename = "~/Desktop/529.png",
-                        initial_comment = "Testing",
-                        channel = "@foobarmaxo"),
-               regexp = "Could not find ")
+  #expect_that(some_function(), is_a("data.frame"))
 
 })
