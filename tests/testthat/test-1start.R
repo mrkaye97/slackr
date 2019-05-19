7b1dfc4a7e524d53ae3a118db5e057b52f9d661f
Daniel P. Egan <dp.egan@gmail.com>
2019-05-19 10:41:35 -0600

    fixing two things

--- tests/testthat/test-1start.R
+++ tests/testthat/test-1start.R
@@ -1,10 +1,13 @@
-source("helpers/helper_loadCredentials.R")
 
-# Some useful references about testing Slack apps/bots
-# Mocking: https://github.com/Skellington-Closet/slack-mock
-# Slack's test api:  https://api.slack.com/methods/api.test
-# Best practices for API packages: https://discuss.ropensci.org/t/best-practices-for-testing-api-packages/460/7
-library(yaml)
-creds <- loadCredentials(service.name = "slackr_test_api", yaml.path = "~/src/slackr/slackr_creds.yaml")
+if(Sys.getenv("SLACK_API_TOKEN") = ""){
+  source("helpers/helper_loadCredentials.R")
 
-Sys.setenv("SLACK_API_TOKEN" = creds$api_token)
+  # Some useful references about testing Slack apps/bots
+  # Mocking: https://github.com/Skellington-Closet/slack-mock
+  # Slack's test api:  https://api.slack.com/methods/api.test
+  # Best practices for API packages: https://discuss.ropensci.org/t/best-practices-for-testing-api-packages/460/7
+  library(yaml)
+  creds <- loadCredentials(service.name = "slackr_test_api", yaml.path = "~/src/slackr/slackr_creds.yaml")
+
+  Sys.setenv("SLACK_API_TOKEN" = creds$api_token)
+}
