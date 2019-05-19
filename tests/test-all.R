47fd0ba17976d4052396754d03ce4bb6e924d5f9
Daniel P. Egan <dp.egan@gmail.com>
2019-05-19 12:01:46 -0600

    moving to dcf format.

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,5 +1,12 @@
 library(testthat)
 library(yaml)
-source(here::here("tests", "helpers", "helper_loadCredentials.R"))
-creds <- loadCredentials(service.name = "slackr_test_api", yaml.path = "~/src/slackr/slackr_creds.yaml")
+library(slackr)
+
+slackr_setup(config_file = system.file(file.path("exdata", "slackr_config.dcf"), package = "slackr"))
+
 test_check("slackr")
+
+# Some useful references about testing Slack apps/bots
+# Mocking: https://github.com/Skellington-Closet/slack-mock
+# Slack's test api:  https://api.slack.com/methods/api.test
+# Best practices for API packages: https://discuss.ropensci.org/t/best-practices-for-testing-api-packages/460/7
--- tests/testthat/test-slackr.R
+++ tests/testthat/test-slackr.R
@@ -3,16 +3,10 @@
 context("basic functioning")
 
 test_that("Webhook fails/works appropriately", {
-  slackr_setup(channel = "#publicchanneltest",
-              incoming_webhook_url = creds$incoming_webhook_url,
-               username = "slackr_bot")
 
   expect_warning(slackr_bot(txt = "testing 1,2,3"))
 
   Sys.sleep(1)
-  slackr_setup(channel = "#publicchanneltest",
-               incoming_webhook_url = creds$incoming_webhook_url,
-               username = "slackr_bot")
 
   expect_error(slackr_bot(txt = "testing 1,2,3"), NA)
 
@@ -22,21 +16,16 @@ test_that("Webhook fails/works appropriately", {
 test_that("Valid api tokens work", {
   # On github, should be done with Env variable.
 
-  slackr_setup(channel = "#publicchanneltest",
-               api_token = Sys.getenv("SLACK_API_TOKEN"),
-               username = "slackr_bot",
-               icon_emoji = "thumbsup")
-
   # Does not respect username, icon, etc.
   expect_error(
-    slackr::slackr_msg(txt = paste("testing ", Sys.time())),
+    slackr::slackr_msg(txt = paste("testing: ", Sys.time())),
                regexp = NA)
   Sys.sleep(1)
   expect_error(res <- slackr::slackr_msg(txt = "testing 1,2,3",
                             channel = "@dp.egan"),
                regexp = NA)
   Sys.sleep(1)
-  expect_error(text_slackr(text = "testing text_slackr",
+  expect_error(text_slackr(text = "testing: text_slackr",
                              icon_emoji = "robot",
                              username = "cashhubbot",
                              channel = "@dp.egan"),
