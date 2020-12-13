89418069b8ab06fc2070f3efadd5fa0d2c96382c
Xinye Li <lixinye@gmail.com>
2020-12-13 15:04:45 +0000

    Fix tex_slackr document pointer to setup notes

--- R/tex_slackr.R
+++ R/tex_slackr.R
@@ -16,7 +16,7 @@
 #' @return \code{httr} response object (invisibly)
 #' @details Please make sure \code{texPreview} package is installed before running this function.
 #'          For TeX setup refer to the
-#'          \href{https://github.com/hrbrmstr/slackr#latex-for-tex_slackr}{installation notes}.
+#'          \href{https://github.com/mrkaye97/slackr#latex-for-tex_slackr}{Setup notes on \code{LaTeX}}.
 #' @examples
 #' \dontrun{
 #' slackr_setup()
