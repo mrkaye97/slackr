5eec32bea57444fed3f318e284b5aa62b8823641
mrkaye97 <mrkaye97@gmail.com>
2021-06-13 13:42:24 -0400

    fixing a broken test

--- tests/testthat/test-zconfig.R
+++ tests/testthat/test-zconfig.R
@@ -1,22 +1,3 @@
-test_that("teardown works", {
-  skip_on_cran()
-  slackr_teardown()
-
-  env_vars <- c(
-    "SLACK_TOKEN",
-    "SLACK_CACHE_DIR",
-    "SLACK_CHANNEL",
-    "SLACK_ICON_EMOJI",
-    "SLACK_INCOMING_WEBHOOK_URL",
-    "SLACK_USERNAME"
-  )
-
-  lapply(
-    env_vars,
-    function(x) expect_equal(Sys.getenv(x), "")
-  )
-})
-
 test_that("config file write works", {
   skip_on_cran()
 
@@ -42,5 +23,25 @@ test_that("config file write works", {
   expect_equal(
     slackr_setup(config_file = tmp),
     "Successfully connected to Slack"
+  )
+})
+
+
+test_that("teardown works", {
+  skip_on_cran()
+  slackr_teardown()
+
+  env_vars <- c(
+    "SLACK_TOKEN",
+    "SLACK_CACHE_DIR",
+    "SLACK_CHANNEL",
+    "SLACK_ICON_EMOJI",
+    "SLACK_INCOMING_WEBHOOK_URL",
+    "SLACK_USERNAME"
+  )
+
+  lapply(
+    env_vars,
+    function(x) expect_equal(Sys.getenv(x), "")
   )
 })
