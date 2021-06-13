831ae755ce99d8a1d144475a59e6f79091461b3a
mrkaye97 <mrkaye97@gmail.com>
2021-06-13 15:53:58 -0400

    test might be breaking

--- tests/testthat/test-connection.R
+++ tests/testthat/test-connection.R
@@ -1,16 +1,3 @@
-test_that("User token throws warnings", {
-  skip_on_cran()
-
-  expect_warning(
-    slackr_setup(
-      token = "xoxp-foo-bar",
-      icon_emoji = "boom",
-      username = "Not Matt"
-    ),
-    "You're using a user token but also specified"
-  )
-})
-
 test_that("Initial setup completes", {
   skip_on_cran()
 
