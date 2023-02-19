c6f3d696e0900b3326f51cf7970adf6bff4b4073
mrkaye97 <mrkaye97@gmail.com>
2023-02-19 13:07:32 -0500

    adds better default args and warning message

--- R/slackr_history.R
+++ R/slackr_history.R
@@ -10,10 +10,10 @@
 #' @param token Authentication token bearing required scopes.
 #' @param channel Channel, private group, or IM channel to send message to. Can be an encoded ID, or a name.
 #' @param posted_from_time Timestamp of the first post time to consider. If both
-#'   posted_to_time and duration is specifed, they take precedence.
+#'   posted_to_time and duration is specifed, they take precedence. (default: 0)
 #' @param duration Number of hours of history to retrieve.  If neither `duration`
 #'   nor `posted_from_time` is specified, there is no time limit on the retrieved
-#'   history.
+#'   history. (default: `NULL`)
 #' @param posted_to_time Timestamp of the last post to consider (default:
 #'   current time).
 #' @param paginate If TRUE, uses the Slack API pagination mechanism, and will retrieve all history inside the timeframe.  Otherwise, makes a single call to the API and retrieves a maximum of `message_count` messages.
@@ -27,16 +27,21 @@ slackr_history <- function(message_count,
                            channel = Sys.getenv("SLACK_CHANNEL"),
                            token = Sys.getenv("SLACK_TOKEN"),
                            posted_to_time = as.numeric(Sys.time()),
-                           duration,
-                           posted_from_time,
+                           duration = NULL,
+                           posted_from_time = 0,
                            paginate = FALSE) {
 
   channel <- slackr_chtrans(channel, token)
 
   if (!missing(duration) && !is.null(duration) && !missing(posted_to_time) && !is.null(posted_to_time)) {
+    if (!missing(posted_from_time) & !is.null(posted_from_time)) {
+      warn(
+        "You specified both a `duration` and a `posted_to_time`. Doing this makes `slackr` infer `posted_from_time`. If you meant to specify a `posted_from_time`, please remove the `duration` you specified.",
+        .frequency = "regularly",
+        .frequency_id = "slackr_history_posted_from_infer_warning"
+      )
+    }
     posted_from_time <- posted_to_time - duration * 3600
-  } else {
-    posted_from_time <- ""
   }
 
   resp <-
