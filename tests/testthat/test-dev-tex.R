f7ef1ab3e8e86babe13b159a6ed4328573df64e1
mrkaye97 <mrkaye97@gmail.com>
2023-02-25 11:16:38 -0500

    run tex tests

--- tests/testthat/test-dev-tex.R
+++ tests/testthat/test-dev-tex.R
@@ -12,10 +12,18 @@ test_that("slackr_dev posts", {
 
 test_that("slackr_tex posts", {
   skip_on_cran()
-  skip_on_ci()
 
+  ## No path specified
   post <- slackr_tex("$$e^{i \\pi} + 1 = 0$$")
 
+  expect_true(post$ok)
+  expect_equal(post$file$filetype, "png")
+
+  ## With path specified
+  d <- tempdir()
+  post <- slackr_tex("$$e^{i \\pi} + 1 = 0$$", path = d)
+
+  unlink(d, recursive = TRUE)
   expect_true(post$ok)
   expect_equal(post$file$filetype, "png")
 })
